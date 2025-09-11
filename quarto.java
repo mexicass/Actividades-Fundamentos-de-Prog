c617da24e7f73c010b561e8741ad35883d7ad7a2
Erik Santos <erik.nelsondias@hotmail.com>
2025-09-10 22:39:28 -0600

    Create quarto.java
    Quarto letrero (Una cajita)

---
+++ quarto.java
@@ -0,0 +1,12 @@
+public class quarto {
+    public static void main(String[] args) {
+        String marco_borde_horizontal = "------------------------";
+        String marco_borde_vertical = "|";
+
+        System.out.print(marco_borde_horizontal + "\n");
+        System.out.print(marco_borde_vertical + "\t\t\t" + marco_borde_vertical + "\n");
+        System.out.print(marco_borde_vertical + "\t\t\t" + marco_borde_vertical + "\n");
+        System.out.print(marco_borde_vertical + "\t\t\t" + marco_borde_vertical + "\n");
+        System.out.print(marco_borde_horizontal + "\n");
+    }
+}
