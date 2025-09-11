c36654101ec07d71163e4bf169f673d32064dabc
Erik Santos <erik.nelsondias@hotmail.com>
2025-09-10 22:37:09 -0600

    Create primer.java
    Mi priemer letrero

---
+++ primer.java
@@ -0,0 +1,8 @@
+public class primer{
+
+    public static void main(String [] args) {
+        System.out.println();
+        System.out.println("Este es mi primer letrero");
+        System.out.println();
+    }//fin main
+}//fin class
