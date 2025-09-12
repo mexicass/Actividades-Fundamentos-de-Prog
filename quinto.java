5824fe2b935591587c7385d22b366cffee0f940a
Erik Santos <erik.nelsondias@hotmail.com>
2025-09-12 16:35:47 -0600

    Create quinto.java
    Mi actividad en clase de letreros en java

---
+++ quinto.java
@@ -0,0 +1,14 @@
+public class Letrero_5{
+
+	public static void main(String [] args){
+	String marco_borde_horizontal= "*************************************************";
+	String marco_borde_vertical="|";
+
+		System.out.print(marco_borde_horizontal+"\n");
+		System.out.print(marco_borde_vertical+"\tTecnologico nacional de mexico\t\t"+marco_borde_vertical+"\n");
+		System.out.print(marco_borde_vertical+"\tDias Dos Santos, Erik Nelson\t\t"+marco_borde_vertical+"\n");
+		System.out.print(marco_borde_vertical+"\t\t\t\t\t\t"+marco_borde_vertical+"\n");
+		System.out.print(marco_borde_horizontal+"\n");
+
+	}
+}
