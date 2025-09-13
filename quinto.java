245c020f1bdd74586b3d7c735d2f0123734dffa8
Erik Santos <erik.nelsondias@hotmail.com>
2025-09-13 01:25:17 -0600

    Update quinto.java
    ahora si quedo mejor y completo

--- quinto.java
+++ quinto.java
@@ -1,14 +1,16 @@
 public class Letrero_5{
 
 	public static void main(String [] args){
-	String marco_borde_horizontal= "*************************************************";
+	String marco_borde_horizontal= "*********************************************************";
 	String marco_borde_vertical="|";
 
-		System.out.print(marco_borde_horizontal+"\n");
-		System.out.print(marco_borde_vertical+"\tTecnologico nacional de mexico\t\t"+marco_borde_vertical+"\n");
-		System.out.print(marco_borde_vertical+"\tDias Dos Santos, Erik Nelson\t\t"+marco_borde_vertical+"\n");
-		System.out.print(marco_borde_vertical+"\t\t\t\t\t\t"+marco_borde_vertical+"\n");
-		System.out.print(marco_borde_horizontal+"\n");
+		System.out.println(marco_borde_horizontal);
+		System.out.println(marco_borde_vertical+"\t\t\t\t\t\t\t"+marco_borde_vertical);
+		System.out.println(marco_borde_vertical+"\tTecnologico Nacional de Mexico\t\t\t"+marco_borde_vertical);
+		System.out.println(marco_borde_vertical+"\tDias Dos Santos, Erik Nelson\t\t\t"+marco_borde_vertical);
+		System.out.println(marco_borde_vertical+"\tIngenieria en Sistemas Computacionales\t\t"+marco_borde_vertical);
+		System.out.println(marco_borde_vertical+"\t\t\t\t\t\t\t"+marco_borde_vertical);
+		System.out.println(marco_borde_horizontal);
 
 	}
 }
