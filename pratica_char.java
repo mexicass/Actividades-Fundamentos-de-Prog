ef1a208960936cf707dec5abdb8ebbc5044dd305
Erik Santos <erik.nelsondias@hotmail.com>
2025-09-17 16:35:51 -0600

    Create pratica_char.java
    nueva pratica de variable

---
+++ pratica_char.java
@@ -0,0 +1,24 @@
+public class pratica_char {
+	
+	public static void main(String [] args){
+	 
+	char barrahorizontal='\u2550';
+	char barravertical='\u2551';
+	char esquinasuperiorizq='\u2554';
+	char esquinainferiorizq='\u255A';
+	char esquinasuperiorder='\u2557';
+	char esquinainfder='\u255D';
+
+	System.out.print( esquinasuperiorizq);
+	System.out.print( barrahorizontal);
+	System.out.println( esquinasuperiorder);
+
+	System.out.println(barravertical+" "+barravertical);
+	System.out.println(barravertical+" "+barravertical);
+
+	System.out.print( esquinainferiorizq);
+	System.out.print( barrahorizontal);
+	System.out.print(esquinainfder+""+"\n");
+
+	}
+}
