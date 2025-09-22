4cba2a9159bcd13833a66fce2d7238197ace31b5
Erik Santos <erik.nelsondias@hotmail.com>
2025-09-22 17:04:44 -0600

    Create calculator.java
    calculadora simple en java

---
+++ calculator.java
@@ -0,0 +1,19 @@
+public class calculator {
+	public static void main(String [] args){
+	
+	final int a = 5;
+	final double b = 7;
+
+	double resta = a - b;
+	double suma = a + b;
+	double multiplicacion = a * b;
+	double division = a / b;
+	
+	
+	System.out.println("El Resultado de la resta es:" + resta);
+	System.out.println("El Resultado de la suma es:" + suma);	
+	System.out.println("El Resultado de la multiplicacion es:" + multiplicacion);
+	System.out.println("El Resultado de la division es:" + division);
+
+	}
+}
