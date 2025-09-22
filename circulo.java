0b33601198307931117caa1ffced9cb2a8c6ffe6
Erik Santos <erik.nelsondias@hotmail.com>
2025-09-22 16:29:40 -0600

    Create circulo.java
    codigo para calcular un circulo

---
+++ circulo.java
@@ -0,0 +1,13 @@
+public class circulo {
+	public static void main(String [] args){
+	
+	final double pi = 3.14159;
+	final int radio = 1;
+
+	double area = pi * radio * radio;
+	double perimetro = 2 * pi * radio;
+	
+	System.out.print(perimetro);
+
+	}
+}
