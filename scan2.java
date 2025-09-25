0baa32102ade287ea26ee26362d72e6cd88d419a
Erik Santos <erik.nelsondias@hotmail.com>
2025-09-24 22:56:04 -0600

    Update scan2.java
    cambios para dejar la caja mas estetica, y funciona hasta 6 digitos

--- scan2.java
+++ scan2.java
@@ -6,7 +6,7 @@ public class scan2 {
 	Scanner entrada = new Scanner (System.in);
 	
 	///sistema de scanner
-	System.out.print("Ingresa el tiempo en segundos: ");
+	System.out.print("Ingresa el tiempo en segundos (Max 6 digitos): ");
 	int segundos = entrada.nextInt();
 	
 
@@ -14,6 +14,7 @@ public class scan2 {
 	int convertir_minutos = segundos / 60;
 	int convertir_horas = segundos / 3600;	
 	
+    entrada.close();
 	
 	///caja
 	char barrahorizontal='\u2500';
@@ -22,20 +23,24 @@ public class scan2 {
 	char esquinainferiorizq='\u2514';
 	char esquinasuperiorder='\u2510';
 	char esquinainfder='\u2518';
+    char esquinamedioizq='\u251c';
+    char esquinamedioder='\u2524';
 
-	System.out.print( esquinasuperiorizq );
-	System.out.print(  barrahorizontal+"\t"+barrahorizontal+"\t"+barrahorizontal+"\t"+barrahorizontal+"\t"+barrahorizontal+"\t"+barrahorizontal+"\t"+barrahorizontal+"\t");
-	System.out.println( esquinasuperiorder);
+	System.out.print(esquinasuperiorizq);
+	System.out.print(barrahorizontal+"\t"+barrahorizontal+"\t"+barrahorizontal+"\t"+barrahorizontal+"\t"+barrahorizontal+"\t"+barrahorizontal+"\t"+barrahorizontal+"\t"+barrahorizontal+"\t");
+	System.out.println(esquinasuperiorder);
 
-	System.out.println(barravertical+ "  Segundos \t" + barravertical + "\tMinutos\t\t" + barravertical + "\tHoras   " + barravertical);	
+	System.out.println(barravertical+ "\tSegundos\t" + barravertical + "\tMinutos \t" + barravertical + "\tHoras \t" + barravertical);
+    
+    System.out.println(esquinamedioizq+""+barrahorizontal+"\t"+barrahorizontal+"\t"+barrahorizontal+"\t"+barrahorizontal+"\t"+barrahorizontal+"\t"+barrahorizontal+"\t"+barrahorizontal+"\t"+barrahorizontal+"\t"+esquinamedioder);
 
-	System.out.println(barravertical+ "    " + segundos + "\t\t" + barravertical + "\t\t\t" +barravertical+ "\t\t" +barravertical);
-	System.out.println(barravertical+ "\t\t" + barravertical + "\t   " + convertir_minutos + " " + "\t\t" +barravertical+ "\t\t" +barravertical);
-	System.out.println(barravertical+ "\t\t" + barravertical + "\t\t\t" + barravertical + "\t"  + convertir_horas + "\t" +barravertical);
+	System.out.println(barravertical+ " \t" + segundos + " \t\t" + barravertical + " \t\t\t" + barravertical + " \t\t" + barravertical);
+	System.out.println(barravertical+ " \t\t\t" + barravertical + " \t" + convertir_minutos + " \t\t" + barravertical + " \t\t" + barravertical);
+	System.out.println(barravertical+ " \t\t\t" + barravertical + " \t\t\t" + barravertical + " \t"  + convertir_horas + " \t" + barravertical);
 	
-	System.out.print( esquinainferiorizq);
-	System.out.print( barrahorizontal+"\t"+barrahorizontal+"\t"+barrahorizontal+"\t"+barrahorizontal+"\t"+barrahorizontal+"\t"+barrahorizontal+"\t"+barrahorizontal+"\t");
-	System.out.print( esquinainfder+" "+"\n");
+	System.out.print(esquinainferiorizq);
+	System.out.print(barrahorizontal+"\t"+barrahorizontal+"\t"+barrahorizontal+"\t"+barrahorizontal+"\t"+barrahorizontal+"\t"+barrahorizontal+"\t"+barrahorizontal+"\t"+barrahorizontal+"\t");
+	System.out.print(esquinainfder+" "+"\n");
 	
 	/*System.out.println("En minutos es:" + segundos + " Segundos" );
 	System.out.println("En minutos es:" + convertir_minutos + " Minutos");
