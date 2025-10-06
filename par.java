dbe5579fcbaa2af0187ae1af47545228d14ed73e
Erik Santos <erik.nelsondias@hotmail.com>
2025-10-06 16:33:08 -0600

    Create par.java
    programa para definir si dos numeros son mayores a 5

---
+++ par.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class par {
+	public static void main(String [] args){
+	
+	Scanner entrada = new Scanner(System.in);
+	System.out.println("Ingresa un valor numerico de X");
+	int x = entrada.nextInt();
+	
+	System.out.println("Ingresa un valor numerico de Y");
+	int y = entrada.nextInt();
+
+	
+	if (x > 4) {
+		if (y > 4)
+		System.out.println("X,Y son mayores a 5");
+	} else {
+		System.out.println("X,Y son menores a 5");
+	}
+}
+}
