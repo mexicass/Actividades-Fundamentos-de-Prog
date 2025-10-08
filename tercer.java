10f764b81da99199581524fb041e0582dba36d2d
Erik Santos <erik.nelsondias@hotmail.com>
2025-10-07 23:52:10 -0600

    Delete tercer.java

--- tercer.java
+++
@@ -1,7 +0,0 @@
-public class tercer {
-    public static void main(String[] args) {
-        System.out.println("\nHola\n estudiantes, \n");
-        System.out.println("\tEste es mi segundo letrero");
-        System.out.println();
-    }//fin main     
-}//fin de class
