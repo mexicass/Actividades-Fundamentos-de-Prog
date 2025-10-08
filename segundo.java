11c7201214fd0617e9933131109426d7a5e1686f
Erik Santos <erik.nelsondias@hotmail.com>
2025-10-07 23:51:59 -0600

    Delete segundo.java

--- segundo.java
+++
@@ -1,7 +0,0 @@
-public class segundo {
-    public static void main(String[] args) {
-        System.out.println();
-        System.out.println("Hola estudiante, ");
-        System.out.println("este es mi primer letrero");
-    }//fin main     
-}//fin de class
