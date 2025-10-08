76b37c3c3b99a0e45ee91b44b9f4ea48ab9f31dc
Erik Santos <erik.nelsondias@hotmail.com>
2025-10-07 23:43:45 -0600

    Update calificaciones.java
    agora sim funciona

--- calificaciones.java
+++ calificaciones.java
@@ -7,28 +7,33 @@ public class par {
 	int cali = entrada.nextInt();
 
 	
-	if (cali > 90) {
-		if (cali < 100)
+	if (cali >= 90) {
+		if (cali <= 100)
 		System.out.println("A");
+        return;
 	}
 	
-	if (cali > 80) { 
-		if (cali < 90) 
+	if (cali >= 80) { 
+		if (cali <= 90) 
 		System.out.println("B");
+        return;
 	} 
 
-	if (cali > 70) {
-		if (cali < 80)
+	if (cali >= 70) {
+		if (cali <= 80)
 		System.out.println("C");
+        return;
 	} 
 	
-	if (cali > 60) {
-		if (cali < 70)
+	if (cali >= 60) {
+		if (cali <= 70)
 		System.out.println("D");
+        return;
 	} 
 
 	else {
 		System.out.println("NA");
+        return;
 	}
 
 	}
