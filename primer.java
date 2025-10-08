9c6788555e3cfd8e609778b43ccb4214fca36733
Erik Santos <erik.nelsondias@hotmail.com>
2025-10-07 23:49:59 -0600

    Delete primer.java

--- primer.java
+++
@@ -1,8 +0,0 @@
-public class primer{
-
-    public static void fundamentos(String [] args) {
-        System.out.println();
-        System.out.println("Este es mi primer letrero");
-        System.out.println();
-    }//fin main
-}//fin class
