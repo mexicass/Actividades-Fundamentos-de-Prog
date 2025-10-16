e28ac4398897ad5956d0dc2ac3d6a0f06ad4de33
mexicass <seuemail@exemplo.com>
2025-10-16 01:56:12 -0600

    ahora funciona perfectamente como el maestro queria, sin ningun error

--- ciclos.java
+++ ciclos.java
@@ -2,32 +2,30 @@ import java.util.Scanner;
 
 public class ciclos {
 	public static void main(String [] args){
+		//Sistema de Scanner 
 		Scanner entrada = new Scanner (System.in);
 		System.out.println("Ingresa el numero: ");
 		int num = entrada.nextInt();
-	 
+		entrada.close();
 
+		//Declarando variables
 		char caracter = '\u2500';
 
-
-
-		for (int i=1; i<21; i++){
-
-		        for (int linea = 1; linea < 10; linea++){
-	        	System.out.print(caracter);
-		}
+		//Haciendo las operaciones
+			for (int linea = 1; linea < 10; linea++){
+	        System.out.print(caracter);
+			}
 			System.out.println();
 
+			for (int i=1; i<21; i++){
 			int resultado = num * i;
-			System.out.println(i + "x" + num + "=" + resultado);
+			System.out.println(num + "x" + i + "=" + resultado);
+			}
 
-		        for (int linea = 1; linea < 10; linea++){
-	        	System.out.print(caracter);
-		}
+			for (int linea = 1; linea < 10; linea++){
+	        System.out.print(caracter);
+	    	}
 			System.out.println();
-        	
-		}
-			
-	
 	}
 }
+
