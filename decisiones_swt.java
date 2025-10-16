c1a7345b9140993c9b92d7fc8daa3556a3fe4a09
mexicass <seuemail@exemplo.com>
2025-10-16 02:36:23 -0600

    algunos cambios que no me acuerdo

--- decisiones_swt.java
+++ decisiones_swt.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class decisiones_swt {
 	public static void main(String [] args){
 		Scanner entrada = new Scanner (System.in);
+
 		System.out.println("Ingresa el numero: ");
 		int num = entrada.nextInt();
 
 		System.out.println("¿Que le gustaria hacer? ");
 		System.out.println("1-Sumar los digitos del numero \n2-Invertir las cifras \n3-Convertir a binario \n4-Convertir a grados centigrados \n5-salir ");
 		int decision = entrada.nextInt ();
+		
 		entrada.close();
 
 		int d1,d2,d3,d4,d5,d6,d7,d8;
@@ -20,17 +22,16 @@ public class decisiones_swt {
   			d3 = (num / 100) % 10; 
 			d2 = (num / 1000) % 10; 
 			d1 = (num / 10000) % 10;
-
 			int resultado = d1+d2+d3+d4+d5;
 			System.out.println(resultado);
         	break;
+
     		case 2:
 			d1 = num % 10;
         	d2 = (num / 10) % 10;
        		d3 = (num / 100) % 10;
         	d4 = (num / 1000) % 10;
         	d5 = (num / 10000) % 10;
-		
 			int invertido =
         	d1 * 10000 +
         	d2 * 1000 +
@@ -39,6 +40,7 @@ public class decisiones_swt {
         	d5;
 			System.out.println(invertido);
         	break;
+
     		case 3:
 			d1 = (num/ (int)Math.pow(2,7)) % 2;
     		d2 = (num/ (int)Math.pow(2,6)) % 2;
@@ -48,10 +50,10 @@ public class decisiones_swt {
     		d6 = (num/ (int)Math.pow(2,2)) % 2;
     		d7 = (num/ (int)Math.pow(2,1)) % 2;
     		d8 = (num/ (int)Math.pow(2,0)) % 2;
-    
     		System.out.println("Este es el numero en binario: " + d1+d2+d3+d4+d5+d6+d7+d8);
 			break;
-		case 4:
+
+			case 4:
 			int celsius = (num - 32) * 5/9;
 			System.out.println("Este es el valor en Celsius: " + celsius);
     		break;
