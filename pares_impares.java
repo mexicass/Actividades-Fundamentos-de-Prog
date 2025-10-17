d367a4e571dba5749464e59c540df71e547264c8
mexicass <seuemail@exemplo.com>
2025-10-17 03:09:28 -0600

    sistema que indentifica numeros, si son positivos o negativos, pares o impares, cero

---
+++ pares_impares.java
@@ -0,0 +1,75 @@
+//importaciones
+import java.util.Scanner;
+
+//abriendo class
+public class pares_impares {
+    public static void main(String [] args){
+
+        //Sistema de Scanner
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingresa el numero a evaluar: ");
+        int num = entrada.nextInt();
+        entrada.close();
+
+        //declarando char's
+        String grafico = "\uD83D\uDCCA";
+        char caracter = '\u2500';
+
+        //Identificando si el numero es positivo o negativo
+        if (num > 0 && num % 2 == 0) {
+            System.out.println(grafico + " Clasificación Compleja de Número Entero " + grafico);
+            System.out.println("\t   Número a evaluar: " + num);
+
+            for (int linea = 0; linea < 45; linea++){
+	            System.out.print(caracter);
+			}
+			System.out.println();
+
+            System.out.println("\tEl numero es: POSITIVO PAR");
+        }
+        else if (num > 0 && num % 2 != 0){
+            System.out.println(grafico + " Clasificación Compleja de Número Entero " + grafico);
+            System.out.println("\t   Número a evaluar: " + num);
+
+            for (int linea = 0; linea < 45; linea++){
+	            System.out.print(caracter);
+			}
+			System.out.println();
+
+            System.out.println("\tEl numero es: POSITIVO IMPAR");
+        }
+        else if (num < 0 && num % 2 == 0){
+            System.out.println(grafico + " Clasificación Compleja de Número Entero " + grafico);
+            System.out.println("\t   Número a evaluar: " + num);
+
+            for (int linea = 0; linea < 45; linea++){
+	            System.out.print(caracter);
+			}
+			System.out.println();
+
+            System.out.println("\tEl numero es: NEGATIVO PAR");
+        }
+        else if (num < 0 && num % 2 != 0){
+            System.out.println(grafico + " Clasificación Compleja de Número Entero " + grafico);
+            System.out.println("\t   Número a evaluar: " + num);
+
+            for (int linea = 0; linea < 45; linea++){
+	            System.out.print(caracter);
+			}
+			System.out.println();
+
+            System.out.println("\tEl numero es: NEGATIVO IMPAR");
+        }
+        else if (num == 0){
+            System.out.println(grafico + " Clasificación Compleja de Número Entero " + grafico);
+            System.out.println("\t   Número a evaluar: " + num);
+
+            for (int linea = 0; linea < 45; linea++){
+	            System.out.print(caracter);
+			}
+			System.out.println();
+
+            System.out.println("\t   El numero es: CERO");
+        }
+    }
+}
